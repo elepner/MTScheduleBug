00e9f4077ef7f25fde86c9a90d4c67a04258c767
edle <eduard.lepner@gmail.com>
2019-02-02 18:48:50 +0100

    Add project files.

---
+++ MT.Scheduling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MT.Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
